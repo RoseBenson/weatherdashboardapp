f72dd546b873a6ede137ac99b253fb2e709e6525
Rosemary Benson <rosebens@outlook.com>
2024-03-06 22:12:34 +0000

    adding to JS

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
   //Api Key 
-  const apiKey = "45dab8e6208f47cd0aa42f480c18d1ce";
+  const APIkey = "45dab8e6208f47cd0aa42f480c18d1ce";
+
+  var queryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}`;
+  console.log(queryUrl);
