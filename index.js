a8e6d8c87da22da5cf4ff3cd7396002d37f2cf83
Rosemary Benson <rosebens@outlook.com>
2024-03-07 16:23:51 +0000

    adding first js

---
+++ index.js
@@ -0,0 +1,18 @@
+ /*
+  //Api Key 
+  const APIkey = "45dab8e6208f47cd0aa42f480c18d1ce";
+
+  var queryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}`;
+  console.log(queryUrl);
+  
+
+  // Api Key
+const APIkey = "45dab8e6208f47cd0aa42f480c18d1ce";
+const apiUrl = "https://api.openweathermap.org/data/2.5/";
+
+*/
+const APIkey = "45dab8e6208f47cd0aa42f480c18d1ce";
+const searchForm = document.getElementById('searchForm');
+const cityInput = document.getElementById('cityInput');
+const currentWeatherDiv = document.getElementById('currentWeather');
+const forecastDiv = document.getElementById('forecast');
